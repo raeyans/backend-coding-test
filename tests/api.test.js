43ed0d683861492ec258a9f126e825ba3ddcef01
Raymond Sihotang <raymond@kawancicil.co.id>
2019-07-28 00:40:32 +0700

    Fix unit test to pass coverage 80%

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,22 +1,18 @@
 'use strict';
 
 const request = require('supertest');
+const assert = require('assert');
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
-
 const app = require('../src/app')(db);
 const buildSchemas = require('../src/schemas');
 
 describe('API tests', () => {
     before((done) => {
-        db.serialize((err) => { 
-            if (err) {
-                return done(err);
-            }
-
+        db.serialize((err) => {
+            if (err) return done(err);
             buildSchemas(db);
-
             done();
         });
     });
@@ -27,6 +23,113 @@ describe('API tests', () => {
                 .get('/health')
                 .expect('Content-Type', /text/)
                 .expect(200, done);
+        });
+    });
+
+    describe('GET /rides', () => {
+        it('should return error could not find any rides', (done) => {
+            request(app)
+                .get('/rides')
+                .expect('Content-Type', /json/)
+                .expect(200, {
+                    error_code: 'RIDES_NOT_FOUND_ERROR',
+                    message: 'Could not find any rides',
+                }, done);
+        });
+    });
+
+    describe('POST /rides', () => {
+        it('should create new ride', (done) => {
+            request(app)
+                .post('/rides')
+                .send({
+                    start_lat: 0,
+                    start_long: 0,
+                    end_lat: 0,
+                    end_long: 0,
+                    rider_name: 'Rider',
+                    driver_name: 'Driver',
+                    driver_vehicle: 'Car',
+                })
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) return done(err);
+                    assert.equal(res.body[0].rideID, 1);
+                    assert.ok(res.body[0].created);
+                    done();
+                });
+        });
+
+        it('should return latitude longitude validation error', (done) => {
+            request(app)
+                .post('/rides')
+                .send({
+                    start_lat: 200,
+                    start_long: 0,
+                    end_lat: 0,
+                    end_long: 0,
+                    rider_name: 'Rider',
+                    driver_name: 'Driver',
+                    driver_vehicle: 'Car',
+                })
+                .expect('Content-Type', /json/)
+                .expect(200, {
+                    error_code: 'VALIDATION_ERROR',
+                    message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
+                }, done);
+        });
+
+        it('should return empty string validation error', (done) => {
+            request(app)
+                .post('/rides')
+                .send({
+                    start_lat: 0,
+                    start_long: 0,
+                    end_lat: 0,
+                    end_long: 0,
+                    rider_name: '',
+                    driver_name: 'Driver',
+                    driver_vehicle: 'Car',
+                })
+                .expect('Content-Type', /json/)
+                .expect(200, {
+                    error_code: 'VALIDATION_ERROR',
+                    message: 'Rider name must be a non empty string',
+                }, done);
+        });
+    });
+
+    describe('GET /rides', () => {
+        it('should return rides', (done) => {
+            request(app)
+                .get('/rides')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) return done(err);
+                    assert.equal(res.body.length, 1);
+                    done();
+                });
+        });
+    });
+
+    describe('GET /rides/{id}', () => {
+        it('should return a ride', (done) => {
+            request(app)
+                .get('/rides/1')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+
+        it('should error could not find any rides', (done) => {
+            request(app)
+                .get('/rides/2')
+                .expect('Content-Type', /json/)
+                .expect(200, {
+                    error_code: 'RIDES_NOT_FOUND_ERROR',
+                    message: 'Could not find any rides',
+                }, done);
         });
     });
 });
