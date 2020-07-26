e0cc66512f22c9397a485fd6fd2c4ced33e0e1e3
Raymond Sihotang <raeyans@gmail.com>
2020-07-27 03:54:50 +0700

    Rollback to remove log

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
 const buildSchemas = require('./src/schemas');
-console.log('Hello World');
 
 db.serialize(() => {
     buildSchemas(db);
